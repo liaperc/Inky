2c956e37a753f163bb308bc5884ded581eaf8c00
Exr0n <mail@exr0n.com>
2023-03-11 20:26:41 -0800

    UT: import robotbase into Main.java??

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import org.usfirst.frc4904.robot.Robot;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
