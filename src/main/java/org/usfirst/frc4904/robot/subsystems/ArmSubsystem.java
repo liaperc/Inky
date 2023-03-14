220b1c6e22c276dd85a44b7db78cbe813a42de34
zbuster05 <zakary.saya@gmail.com>
2023-03-14 12:27:49 -0500

    Revert "false"
    This reverts commit 87cda0b5dc5c733ffb837962845b5a8dbfa14d19.

---
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc4904.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+public class ArmSubsystem {
+    public final ArmPivotSubsystem pivotArmSubsystem;
+    public final ArmExtensionSubsystem armExtensionSubsystem;
+    public ArmSubsystem(ArmPivotSubsystem pivotArmSubsystem, ArmExtensionSubsystem armExtensionSubsystem) {
+        this.pivotArmSubsystem = pivotArmSubsystem;
+        this.armExtensionSubsystem = armExtensionSubsystem;
+    }
+    
+    public ParallelCommandGroup c_holdArmPose(double degreesFromHorizontal, double extensionLengthInches) {
+        var cmdgrp = new ParallelCommandGroup(
+            pivotArmSubsystem.c_holdRotation(degreesFromHorizontal),
+            armExtensionSubsystem.c_holdExtension(extensionLengthInches)
+        );
+        cmdgrp.addRequirements(pivotArmSubsystem, armExtensionSubsystem);
+        return cmdgrp;
+    }
+
+
+}
