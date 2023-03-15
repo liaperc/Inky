fc8926b8905dbcfb195a6de8d6050d87d987e12d
zbuster05 <zakary.saya@gmail.com>
2023-03-15 15:36:32 -0500

    Sequential stuff

--- src/main/java/org/usfirst/frc4904/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/arm/ArmSubsystem.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc4904.robot.subsystems.arm;
 
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class ArmSubsystem {
     public final ArmPivotSubsystem pivotArmSubsystem;
@@ -18,9 +23,26 @@ public class ArmSubsystem {
     }
 
     public ParallelCommandGroup c_holdArmPose(double degreesFromHorizontal, double extensionLengthInches) {
+        Command firstCommand;
+        Command secondCommand;
+        double wait;
+
+        Pair<Command, Double> pivotMovement = pivotArmSubsystem.c_holdRotation(degreesFromHorizontal, MAX_VELOCITY_PIVOT, MAX_ACCEL_PIVOT);
+        Pair<Command, Double> extensionMovement = armExtensionSubsystem.c_holdExtension(extensionLengthInches, MAX_VELOCITY_EXTENSION, MAX_ACCEL_EXTENSION);
+
+        if ((extensionLengthInches - armExtensionSubsystem.getCurrentExtensionLength()) > 0) {
+            firstCommand = pivotMovement.getFirst();
+            wait = pivotMovement.getSecond();
+            secondCommand = extensionMovement.getFirst();
+        } else {
+            firstCommand = extensionMovement.getFirst();
+            wait = extensionMovement.getSecond();
+            secondCommand = pivotMovement.getFirst();
+        }
+
         var cmdgrp = new ParallelCommandGroup(
-            pivotArmSubsystem.c_holdRotation(degreesFromHorizontal, MAX_VELOCITY_PIVOT, MAX_ACCEL_PIVOT).getFirst(),
-            armExtensionSubsystem.c_holdExtension(extensionLengthInches, MAX_VELOCITY_EXTENSION, MAX_ACCEL_EXTENSION).getFirst()
+            firstCommand,
+            new SequentialCommandGroup(new WaitCommand(wait * 1000), secondCommand);
         );
         cmdgrp.addRequirements(pivotArmSubsystem, armExtensionSubsystem);
         return cmdgrp;
