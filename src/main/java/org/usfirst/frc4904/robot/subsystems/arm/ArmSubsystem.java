7f2c56f9c520eabda7f481d924714b7685ee51d2
aze12345 <88347612+aze12345@users.noreply.github.com>
2023-03-31 15:00:35 -0700

    oops

--- src/main/java/org/usfirst/frc4904/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/arm/ArmSubsystem.java
@@ -113,7 +113,7 @@ public class ArmSubsystem extends SubsystemBase {
             .alongWith(new WaitCommand(1).andThen(RobotMap.Component.intake.c_holdVoltage(Intake.DEFAULT_INTAKE_VOLTS))
         )).withTimeout(5); //TODO: change timeout
     }
-    public Command c_angleCones(int shelf) {
+    public Command placeCones(int shelf) {
         var degreesFromHorizontal = cones.get(shelf).getFirst();
         var extensionLengthMeters = cones.get(shelf).getSecond();
         
