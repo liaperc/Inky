3fb4c2f566a45000e087c540d185e50e56a77e49
roboticsteam4904-2 <botprovoking@gmail.com>
2023-03-31 16:29:33 -0700

    Tuned driver gain

--- src/main/java/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
@@ -11,7 +11,7 @@ public class NathanGain extends Driver {
 	public static final double SPEED_EXP = 2;
 	public static final double Y_SPEED_SCALE = 1;
 
-	public static final double TURN_EXP = 3.3;
+	public static final double TURN_EXP = 4;
 	public static final double TURN_SPEED_SCALE = 1;
 
 
@@ -19,7 +19,7 @@ public class NathanGain extends Driver {
 	public static final double NORMAL_TURN_GAIN = 0.3;
 
 	public static final double PRECISE_SPEED_SCALE = 0.4;	
-	public static final double PRECISE_TURN_SCALE = 0.3;
+	public static final double PRECISE_TURN_SCALE = 0.1;
 
 	// public static final double SPEEDY_TURN_GAIN = 0.7;
 
